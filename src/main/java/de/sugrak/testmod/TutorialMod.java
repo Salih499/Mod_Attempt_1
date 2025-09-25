8dd398e5a7282af081a642689628402e13331dcd
Salih-Denizer Ugrak <sdugrak@yahoo.de>
2025-09-25 17:09:03 +0200

    New Mod

--- src/main/java/de/sugrak/testmod/TutorialMod.java
+++ src/main/java/de/sugrak/testmod/TutorialMod.java
@@ -1,7 +1,10 @@
 package de.sugrak.testmod;
 
 import com.mojang.logging.LogUtils;
+import de.sugrak.testmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.CreativeModeTabRegistry;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
@@ -36,7 +39,7 @@ public class TutorialMod
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
-
+        ModItems.register(modEventBus);
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -52,7 +55,9 @@ public class TutorialMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+    if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+        event.accept(ModItems.EREN);
+    }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
